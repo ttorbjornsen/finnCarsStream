80bcfb75e13ee987615aa1880b86ad66e3dd842f
torbjorn.torbjornsen <ttorbjornsen@gmail.com>
2016-07-08 12:45:47 +0200

    temp

--- src/main/scala/KafkaProducer.scala
+++ src/main/scala/KafkaProducer.scala
@@ -24,6 +24,7 @@ object KafkaProducer extends App{
   val hdrPages1 = Range(1,3,1)
   val hdrPages2 = Range(3,5,1)
 
+  // each Future will async try to extract page results from Kafka and
 
   val f1:Future[Unit] = Future{
     hdrPages1.map{page =>
@@ -41,29 +42,17 @@ object KafkaProducer extends App{
     }
   }
 
-  //val fSer: Future[Unit] = f1.flatMap(res1 => f2)
+  //ensure that the main thread will not end before each Future has been completed. Timeout after 10 minutes.
   val fSer2: Future[Unit] = for {
     r1 <- f1
     r2 <- f2
   } yield(r1,r2)
-
   Await.result(fSer2, 10 minutes) //avoid main thread stopping before futures have completed
+
+  // TODO: print message when some pages have not been loaded successfully (due to timeout)?
   fSer2.onComplete{
-    case Success(_) => println("Finished writing pages " + Math.min(hdrPages1.min, hdrPages2.min) + " - " + Math.max(hdrPages1.max, hdrPages2.max) + " to Kafka")
+    case Success(_) => println("Finished trying to write pages " + Math.min(hdrPages1.min, hdrPages2.min) + " - " + Math.max(hdrPages1.max, hdrPages2.max) + " to Kafka")
     case Failure(_) => println("Timeout - not able to complete writing to Kafka within time limit.")}
-
-
-
-
-
-  //  hdrPages.foreach { page =>
-////    Thread.sleep(2000)
-//    println("Page + " + page + " - start extracting")
-//    val url = "https://extraction.import.io/query/extractor/048ad769-f701-42a4-9ed3-5593b3ed0cdc?_apikey=6f684a69407b449fa8d1a8ea294f5a6bd9e7c426d14291abefb6c0dbc492734acac43b1be257f10739a580c935718ae96d03d3da27d0a4da95c2d6b411a65707583f18d38ae3ccdfd62cc74ff4791e27&url=http%3A%2F%2Fm.finn.no%2Fcar%2Fused%2Fsearch.html%3Fbody_type%3D4%26rows%3D100%26page%3D" + page
-//    Utility.saveFinnCarsPageResults(producer, topic, url)
-//    }
-
-
 }
 
 
